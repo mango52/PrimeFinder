c90c4e85879e337bc68f65cd3b9716dff06e2b90
mangodev <sam.rankin@me.com>
2012-09-15 22:41:39 -0400

    auto-cleanup

--- src/me/mango/primefinder/PrimeFinder.java
+++ src/me/mango/primefinder/PrimeFinder.java
@@ -1,8 +1,8 @@
 package me.mango.primefinder;
 
 public class PrimeFinder {
-	
-	//TODO: Multithreading
+
+	// TODO: Multithreading
 
 	private static int maxNumber;
 	private static int primes = 0;
@@ -10,26 +10,26 @@ public class PrimeFinder {
 	public static void main(String args[]) {
 		try {
 			maxNumber = Integer.parseInt(args[0]);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			maxNumber = 100000;
 			System.out.println("No max number specified - using 100,000");
 		}
 
-		for(int i = 2; i <= maxNumber; i++) {
+		for (int i = 2; i <= maxNumber; i++) {
 			boolean prime = true;
-			for(int a = i-1; a >= 2; a--) {
-				if(((int) ((double) i / (double) a) * (double) a) == i) {
+			for (int a = i - 1; a >= 2; a--) {
+				if (((int) ((double) i / (double) a) * (double) a) == i) {
 					prime = false;
 					break;
 				}
 			}
-			if(prime) {
+			if (prime) {
 				primes++;
 				System.out.println(i);
 			}
 		}
 
 		System.out.println("Calculations complete");
-		System.out.println("Found " + primes + " primes in " + maxNumber +" numbers (" + ((double) primes / (double) maxNumber) * 100 + "%)");
+		System.out.println("Found " + primes + " primes in " + maxNumber + " numbers (" + (((double) primes / (double) maxNumber) * 100) + "%)");
 	}
 }
